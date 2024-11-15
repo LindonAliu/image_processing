c893bb52c2039d27d331b11eaf5aef6ff59cccf6
Cassandre MOTTE <cassandre.motte@phelma.grenoble-inp.fr>
2024-11-15 13:55:36 +0900

    feat(geo): main added to run and see photo

--- src/main.py
+++ src/main.py
@@ -4,12 +4,27 @@
 ## File description:
 ## main
 ##
+from geometry import img_to_fisheye
+import cv2
 
 from app import AppState
 
 def main():
     app: AppState = AppState()
     app.create_gui().run()
+
+    file = "team13_photo.jpg"
+    dest = img_to_fisheye(file, 0.00005)
+
+    cv2.imshow("Original",cv2.imread(file))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    cv2.imshow("Barrel Distortion Effect", dest)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
 
 if __name__ == '__main__':
     main()
